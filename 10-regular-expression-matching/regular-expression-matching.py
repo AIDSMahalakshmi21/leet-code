88f03eb7b37ea4657a6ab6cd6ba74a0ad463e3cc
AIDSMahalakshmi21 <mahamaha21092007@gmail.com>
2026-07-27 21:21:49 +0530

    Time: 6 ms (87.73%) | Memory: 12.8 MB (26.13%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isMatch(self, s, p):
+        memo = {}
+
+        def dfs(i, j):
+            
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if j == len(p):
+                return i == len(s)
+
+            first_match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+
+            if (j + 1) < len(p) and p[j + 1] == '*':
+                
+                res = dfs(i, j + 2) or (first_match and dfs(i + 1, j))
+            
+            else:
+                res = first_match and dfs(i + 1, j + 1)
+
+            memo[(i, j)] = res
+            return res
+
+        return dfs(0, 0)
